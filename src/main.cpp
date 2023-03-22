def07d60ab8237d7dd4cd8fcc817d7567841e86c
Ashram56 <vnguyenquangdo@gmail.com>
2023-03-22 18:28:43 +0100

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -29,62 +29,65 @@
 // Some logic to analyse:
 #include "hardware/structs/pwm.h"
 
-#define LED_COUNT 8
+#define LED_COUNT 35
 #define LED_PIN 10
 #define IOSTB_PIN 15
 
+#define PICO_DEFAULT_LED_PIN 16
+#define SAM_DETECTOR_VERSION 100
+
+
 WS2812FX ws2812fx = WS2812FX(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-// Read bus on pio0
+//------------------------------------------------------------------------------
+// Local data
 
+// Read bus on pio0
 static PIO sPioReadData = pio0;
+
 static int sSmRead = -1;
 static int sSmReadBusOffset = -1;
+static uint32_t sBusData = 0 ;
+static uint8_t numBusData = 0;
+
+static bool noBusData = false;
+static uint32_t bus_data = 0;
+
+static char msg[32];
 
 //------------------------------------------------------------------------------
 
 bool pio_init()
 {
-    // Input/Blanking on PIO 0
+    // Input on PIO0
 
     // Find a place for the PIO program in the instruction memory
     sSmReadBusOffset = pio_add_program(sPioReadData, &read_bus_data_program);
-    // Claim an unused state machine for the column reading and run the program
+    // Claim an unused state machine for data reading and run the program
     sSmRead = pio_claim_unused_sm(sPioReadData, true);
     read_bus_data_program_init(sPioReadData, sSmRead, sSmReadBusOffset, IOSTB_PIN);
   
     return ((sSmRead != -1));
 }
 
-uint16_t pio_read_bus_data()
+uint32_t pio_read_bus_data()
 {
     // Process all new data
-    bool noBusData = false;
-    uint8_t numBusData = 0;
-        bool waited = false;
-    while (!noBuslData )
+       
+    while (!noBusData)
     {
-        // Check for new row data
-        if (sSmRow != -1)
+        // Check for new bus  data
+        if (sSmRead != -1)
         {
-            noRowData = pio_sm_is_rx_fifo_empty(sPioColRow, sSmRow);
-            if (!noRowData)
+            noBusData = pio_sm_is_rx_fifo_empty(sPioReadData, sSmRead);
+            if (!noBusData)
             {
-                uint32_t d = pio_sm_get(sPioColRow, sSmRow);
-                sRowDataPrel = (uint8_t)((d >> 23) & 0xff);
-                numRowData++;
+                sBusData = pio_sm_get(sPioReadData, sSmRead);
             }
         }
-
-            // wait a bit for new data to arrive - all input should happen
-        // within a few microseconds
-        if (!waited && noColData && noRowData && ((numColData + numRowData) < 4))
-        {
-            waited = true;
-            noColData = false;
-            sleep_us(6);
-        }
     }
+    return (sBusData);
+}
 
 void panic_mode()
 {
@@ -96,6 +99,19 @@ void panic_mode()
     }
 }
 
+void sam_init()
+{
+    Serial.printf("SAM Detector \n", SAM_DETECTOR_VERSION);
+}
+
+uint32_t sam_process_data() {
+
+    // read data from PIO
+    uint32_t inData = pio_read_bus_data();
+    return(inData);
+
+}
+
 void setup() {
 
     Serial.begin(115200);
@@ -105,23 +121,40 @@ void setup() {
     ws2812fx.setSpeed(200);
     ws2812fx.setMode(FX_MODE_CHASE_FLASH);
     ws2812fx.start();
-  
+
     delay(500);
-    Serial.printf("Starting\n");
+    Serial.printf("\nStern SAM Detector v0.1\n");
 
-     // PIO setup
+    // initialize the Pi Pico's LED
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
 
+    // PIO setup
     if (!pio_init())
     {
         Serial.printf("Failed to initialize the PIOs!\n");
         panic_mode();
-    } else {  ws2812fx.setMode(FX_MODE_COMET); }
+    }
+
+    sam_init();
 
 }
 
 void loop() {
     
-    ws2812fx.service();
-    Serial.printf("Looping\n");
+        // every device needs a blinking LED
+        gpio_put(PICO_DEFAULT_LED_PIN, 1);
+        sleep_ms(100);
+        gpio_put(PICO_DEFAULT_LED_PIN, 0);
+        sleep_ms(100);
 
+        bus_data = sam_process_data();
+
+        Serial.printf("\nBus data=");
+        sprintf(msg,"%ld",bus_data);
+        Serial.printf(msg);
+
+        ws2812fx.service();
+       
+    
 }
